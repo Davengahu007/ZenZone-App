05045cca88898b9e0274fd5effec25748424349a
davengahu007 <davengahu007@gmail.com>
2023-06-17 22:20:52 +0300

    Second Commit

--- App.js
+++ App.js
@@ -5,7 +5,6 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 import Home from './screens/Home'
 
-
 const Stack = createNativeStackNavigator()
 
 export default function App() {
